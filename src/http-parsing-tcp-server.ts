975565696dad98ab283e91d582391d70c4696d04
Vignesh D <vignesh.d9123@gmail.com>
2025-09-13 20:24:57 +0530

    Minor format

--- src/http-parsing-tcp-server.ts
+++ src/http-parsing-tcp-server.ts
@@ -1,12 +1,12 @@
 import net from 'net'
 
-const server = net.createServer((socket)=>{
-    socket.on('data', (data)=>{
+const server = net.createServer((socket) => {
+    socket.on('data', (data) => {
         let dataString = data.toString()
         const firstLine = dataString.split('\r\n')[0] // \r\n is the CRLF in HTTP RFC
-        const [method, path] =  firstLine.split(" ")
+        const [method, path] = firstLine.split(" ")
         console.log("Method", method, "Path", path)
-        if(method == "GET"){
+        if (method == "GET") {
             socket.write(`HTTP/1.1 200 OK
 Date: Fri, 12 Sep 2025 16:45:00 GMT
 Server: Apache/2.4.57 (Unix)
@@ -19,10 +19,10 @@ Connection: Closed
   "name":"Vignesh"
 }
 `)
-socket.end()
+            socket.end()
         }
 
-        if(method == "POST"){
+        if (method == "POST") {
             socket.write(`HTTP/1.1 404 NOT FOUND
 Date: Fri, 12 Sep 2025 16:45:00 GMT
 Server: Apache/2.4.57 (Unix)
@@ -32,13 +32,13 @@ Content-Length: 20
 Connection: Closed
 
 Method not found`)
-                socket.end()
+            socket.end()
         }
 
 
     })
 })
 
-server.listen(3000, ()=>{
+server.listen(3000, () => {
     console.log("Server running")
 })
